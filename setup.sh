3abcef64c7d73a639ff10fd49027e73d0b0f9eac
DrJupiter <klausjupiter@hotmail.com>
2024-11-21 11:26:19 +0100

    added setup and data ignore

---
+++ setup.sh
@@ -0,0 +1,9 @@
+pipx install pre-commit
+pre-commit install
+
+echo "Setup precommit"
+
+mkdir data/datafiles
+git clone git@github.com:brendenlake/SCAN.git data/datafiles
+
+echo "Installed the dataset"
