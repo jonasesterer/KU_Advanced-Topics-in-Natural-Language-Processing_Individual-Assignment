b012078baa168df3970fdb0da18d57014565f626
DrJupiter <klausjupiter@hotmail.com>
2024-12-14 15:40:39 +0100

    Updated setup files for linux and windows

---
+++ linux_setup.sh
@@ -0,0 +1,9 @@
+pipx install pre-commit ruff
+pre-commit install
+
+echo "Setup precommit"
+
+mkdir data/datafiles
+git clone https://github.com/brendenlake/SCAN.git data/datafiles
+
+echo "Installed the dataset"
