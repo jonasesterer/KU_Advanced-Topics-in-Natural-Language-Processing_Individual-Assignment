29f5b21c5cf792565f19538c9fc6a73ae0fc8176
DrJupiter <klausjupiter@hotmail.com>
2024-12-16 11:20:00 +0100

    Updated the training and evaluation scripts for experiment 3

--- evaluate.sh
+++ evaluate.sh
@@ -6,4 +6,5 @@ echo "Evaluating experiment 2"
 python3 experiments/evaluate.py ./results_ex2/ 2
 
 echo "Evaluating experiment 3"
-python3 experiments/evaluate.py ./results_ex3/ 3
+python3 experiments/evaluate.py ./results_ex3_p1/ 3
+python3 experiments/evaluate.py ./results_ex3_p2/ 3
--- train.sh
+++ train.sh
@@ -12,12 +12,15 @@ echo "Training models for experiment 2"
 python3 experiments/train.py 2 ./data/datafiles/length_split/tasks_train_length.txt ./data/datafiles/length_split/tasks_test_length.txt ./results_ex2
 
 echo "Training models from experiment 3"
-python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num1_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num1_rep1.txt ./results_ex3
-python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num2_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num2_rep1.txt ./results_ex3
-python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num4_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num4_rep1.txt ./results_ex3
-python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num8_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num8_rep1.txt ./results_ex3
-python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num16_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num16_rep1.txt ./results_ex3
-python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num32_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num32_rep1.txt ./results_ex3
+python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num1_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num1_rep1.txt ./results_ex3_p2
+python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num2_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num2_rep1.txt ./results_ex3_p2
+python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num4_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num4_rep1.txt ./results_ex3_p2
+python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num8_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num8_rep1.txt ./results_ex3_p2
+python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num16_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num16_rep1.txt ./results_ex3_p2
+python3 experiments/train.py 3 ./data/datafiles/add_prim_split/with_additional_examples/tasks_train_addprim_complex_jump_num32_rep1.txt ./data/datafiles/add_prim_split/with_additional_examples/tasks_test_addprim_complex_jump_num32_rep1.txt ./results_ex3_p2
+
+python3 experiments/train.py 3 ./data/datafiles/add_prim_split/tasks_train_addprim_turn_left.txt ./data/datafiles/add_prim_split/tasks_test_addprim_turn_left.txt ./results_ex3_p1
+python3 experiments/train.py 3 ./data/datafiles/add_prim_split/tasks_train_addprim_jump.txt ./data/datafiles/add_prim_split/tasks_test_addprim_jump.txt ./results_ex3_p1
 
 echo "Finished training."
 echo "Run the eval.sh file to get evaluation results"
