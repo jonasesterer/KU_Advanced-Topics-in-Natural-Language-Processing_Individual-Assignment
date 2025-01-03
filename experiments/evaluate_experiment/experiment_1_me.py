199e52eb599b2fac89dfc97b8a05417a88982a24
Jonas Béla Esterer <115395530+jonasesterer@users.noreply.github.com>
2025-01-03 16:31:26 +0100

    Update experiment_1_me.py

--- experiments/evaluate_experiment/experiment_1_me.py
+++ experiments/evaluate_experiment/experiment_1_me.py
@@ -75,6 +75,7 @@ def evaluate(
             print(f"target size 1: {target_ids.size(1)}")
             print(f"target size -1: {target_ids.size(-1)}")
             outputs = model.generate(input_ids, max_length=target_ids.size(1))
+            print(outputs)
 
             # Decode target and prediction
             tgt_decoded = [
