c6deadf25f353f3dd69be9347e91892ebc10134f
Jonas Béla Esterer <115395530+jonasesterer@users.noreply.github.com>
2025-01-03 11:35:25 +0100

    Create evaluate_me.py

---
+++ experiments/evaluate_me.py
@@ -0,0 +1,92 @@
+import torch
+from transformers import T5Tokenizer, T5ForConditionalGeneration, set_seed
+from experiments.tokenizer_dataloader import SCANDataset
+#from tokenizers import Tokenizer
+from torch.utils.data import DataLoader
+#from models.transformer import Transformer
+from pathlib import Path
+from experiments.experiment import ConfigExperiment
+#from transformers import set_seed
+from typing import Union, Dict
+
+
+def main(models_folder: str, experiment_number: int):
+    set_seed(0)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    #path_tokenizer = str(Path(__file__).parent.parent / "custom_tokenizer.json")
+    #tokenizer: Tokenizer = Tokenizer.from_file(path_tokenizer)
+
+    models_folder_path = Path(models_folder)
+
+    # TODO in these we can have an expected results that is contingent on the
+    # label, si
+    # plot should take an optional argument, which is expected results
+    match experiment_number:
+        case 1:
+            from experiments.evaluate_experiment.experiment_1_me import (
+                extract_label_from_path,
+                evaluate,
+                plot,
+            )
+        case 2:
+            from experiments.evaluate_experiment.experiment_2 import (
+                extract_label_from_path,
+                evaluate,
+                plot,
+            )
+        case 3:
+            from experiments.evaluate_experiment.experiment_3 import (
+                extract_label_from_path,
+                evaluate,
+                plot,
+            )
+        case _:
+            raise NotImplementedError(
+                f"Evaluation is not implemented for experiment {experiment_number}"
+            )
+
+    results = {}
+
+    for model_file in models_folder_path.glob("*-model-*.pt"):
+        config_file = Path(
+            str(model_file).replace("model", "config").replace(".pt", ".json")
+        )
+        config = ConfigExperiment.from_pretrained(config_file)
+        # ? with or without config ? 
+        model = load_model(model_file, device)
+
+        test_file = config.training.file_path_test
+
+        dataset_test = SCANDataset(test_file)
+        dataloader_test = DataLoader(dataset_test, batch_size=1024, shuffle=False)
+        # ? Dataset or DataLoader ?
+        label = extract_label_from_path(test_file)
+        results[label] = evaluate(model, dataloader_test, device)
+
+    plot(results)
+
+
+def load_model(
+    model_path: Path, device: torch.device
+) -> T5ForConditionalGeneration: #Transformer:
+    #model = Transformer(**config.model.model_dump())
+    T5ForConditionalGeneration.from_pretrained("t5-small")
+    state_dict = torch.load(model_path, map_location=device, weights_only=True)
+    model.load_state_dict(state_dict)
+    model = model.to(device)
+    model.eval()
+    return model
+
+
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+
+    assert len(args) == 2, "Provide: <PATH-MODELS+CONFIGS> <EXPERIMENT-NUMBER>"
+
+    models_folder = sys.argv[1]
+    experiment_number = int(sys.argv[2])
+
+    main(models_folder, experiment_number)
